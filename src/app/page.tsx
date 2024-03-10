ce28f76a0e0fc8d3f4c5068d61807c3acf014049
ShivharakhYadav <yadavshivharakhyadav@gmail.com>
2024-03-10 23:05:26 +0530

    Carousel and Inspired Section Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,35 @@
 import React, { useState } from 'react';
 import { usePathname } from 'next/navigation';
 
-import { Menu, Layout, Typography } from 'antd';
+import {
+  Menu,
+  Layout,
+  Typography,
+  Carousel,
+  Image,
+  Card,
+  Row,
+  Col,
+} from 'antd';
 import type { MenuProps } from 'antd';
 import { StyleProvider } from '@ant-design/cssinjs';
-import { BarsOutlined } from '@ant-design/icons';
+import {
+  BarsOutlined,
+  BulbOutlined,
+  LeftCircleOutlined,
+  RightCircleOutlined,
+} from '@ant-design/icons';
+
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { SwiperOptions } from 'swiper/types';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 const { Content } = Layout;
 
 const items: MenuProps['items'] = [
@@ -35,6 +60,12 @@ const items: MenuProps['items'] = [
     className: 'd',
   },
 ];
+
+type inspireType = {
+  icon: any;
+  title: string;
+  paragraph: string;
+};
 
 const App: React.FC = () => {
   const u = usePathname();
@@ -87,6 +118,109 @@ const App: React.FC = () => {
           />
         )}
       </div>
+
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        slidesPerView={1}
+        spaceBetween={10}
+        navigation
+        // navigation={{
+        //   nextEl: '.swiper-button-next',
+        //   prevEl: '.swiper-button-prev',
+        //   enabled: true,
+        // }}
+        pagination={{ clickable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+        loop
+        className='h-96 swiper'
+        breakpoints={{
+          320: {
+            slidesPerView: 1,
+            // spaceBetween: 20,
+          },
+          480: {
+            slidesPerView: 1,
+            // spaceBetween: 30,
+          },
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
+      >
+        <SwiperSlide>
+          <img
+            src='/1.jpg'
+            className='h-full w-full object-cover'
+            // className='h-full w-full min-[420px]:object-cover sm:object-cover md:object-cover lg:object-cover xl:object-cover 2xl:object-cover'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src='/2.jpg'
+            className='h-full w-full object-cover'
+            // className='h-full w-full min-[420px]:object-cover sm:object-cover md:object-cover lg:object-cover xl:object-cover 2xl:object-cover'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src='/1.jpg'
+            className='h-full w-full object-cover'
+            // className='h-full w-full min-[420px]:object-cover sm:object-cover md:object-cover lg:object-cover xl:object-cover 2xl:object-cover'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src='/2.jpg'
+            className='h-full w-full object-cover'
+            // className='h-full w-full min-[420px]:object-cover sm:object-cover md:object-cover lg:object-cover xl:object-cover 2xl:object-cover'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src='/1.jpg'
+            className='h-full w-full object-cover'
+            // className='h-full w-full min-[420px]:object-cover sm:object-cover md:object-cover lg:object-cover xl:object-cover 2xl:object-cover'
+          />
+        </SwiperSlide>
+
+        {/* <div className='swiper-button-prev'>
+          <button>Test</button>
+        </div>
+        <div className='swiper-button-next'>
+          <button>Test</button>
+        </div> */}
+      </Swiper>
+
+      <div style={{ marginTop: 20, padding: '0 20px' }}>
+        <Row gutter={[16, 16]}>
+          {inspiredArray?.map((item) => {
+            return (
+              <Col
+                xs={24}
+                sm={12}
+                md={8}
+                lg={8}
+                xl={6}
+                xxl={8}
+                key={item.title}
+              >
+                <InspireCard item={item} />
+              </Col>
+            );
+          })}
+        </Row>
+      </div>
     </Content>
   );
 };
@@ -95,8 +229,65 @@ export default App;
 
 const Title = () => {
   return (
-    <Typography style={{ color: 'white', fontSize: 20 }}>
+    <Typography
+      style={{
+        color: 'white',
+        fontSize: 25,
+        fontWeight: 'bold',
+      }}
+    >
       Raghuvir Foundation
     </Typography>
   );
 };
+
+const InspireCard = ({ item }: { item: inspireType }) => {
+  return (
+    <Card
+      style={{ textAlign: 'center', fontSize: 40 }}
+      styles={
+        {
+          // body: {
+          //   maxWidth: 300,
+          // },
+        }
+      }
+    >
+      {/* {item.icon} */}
+      <BarsOutlined />
+      <Typography.Title level={3} style={{ margin: 0 }}>
+        {item.title}
+      </Typography.Title>
+      <Typography.Title level={5} style={{ margin: 0 }}>
+        {item.paragraph}
+      </Typography.Title>
+    </Card>
+  );
+};
+
+const inspiredArray: Array<inspireType> = [
+  {
+    icon: '',
+    title: 'Get Inspired',
+    paragraph:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
+  },
+  {
+    icon: '',
+    title: 'Get Inspired',
+    paragraph:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
+  },
+  {
+    icon: '',
+    title: 'Get Inspired',
+    paragraph:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
+  },
+  {
+    icon: '',
+    title: 'Get Inspired',
+    paragraph:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
+  },
+];
